db21884761fd9d3d3d8602e28d35181af2035184
kayman1689 <72843270+kayman1689@users.noreply.github.com>
2021-01-28 17:38:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,23 +11,26 @@ function setup() {
   database=firebase.database();
 dog = createSprite(250, 300, 150, 150);
 dog.addImage(dogimage);
-dog.scale = 0.4;
+dog.scale = 0.2;
 foodStock=database.ref('Food');
 foodStock.on("value",readStock);
 }
 
 
 function draw() {  
-background(46, 139, 87);
-if(keyWentDown(UP_ARROW)){
-  writeStock(foodS);
-  dog.addImage(dogHappy);
+ background(46, 139, 87);
+ fill("white");
+ text("Food remaining:"+foodS, 170,150);
+ text("Press Up Arrow To Feed Dog", 130,10,300,20);
+ if(keyWentDown(UP_ARROW)){
+   writeStock(foodS);
+   dog.addImage(dogimageone);
+ }
+   drawSprites();
+   //add styles here
 }
-  drawSprites();
-  //add styles here
-}
-function readStock(data){
-  foodS=data.val();
+  function readStock(data){
+   foodS=data.val();
 }
 function writeStock(x){
   if(x<=0){
@@ -36,9 +39,11 @@ function writeStock(x){
     x=x-1;
   }
   database.ref('/').update({
-    food:x
+    Food: x
   })
 }
+
+
 
 
 
